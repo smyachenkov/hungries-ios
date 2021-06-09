062ca40276c9e7d455ccc333d6cd60536bd77618
smyachenkov <s.myachenkov@gmail.com>
2021-06-09 11:32:30 +0300

    Initial Commit

---
+++ Hungries/HungriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HungriesApp.swift
+//  Hungries
+//
+//  Created by Stanislav Miachenkov on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct HungriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
